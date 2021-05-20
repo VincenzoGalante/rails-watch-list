62bee2461fac6f66f7f0bec58f373adf281d3c8f
Vincenzo Galante <mr.vincenzo.galante@gmail.com>
2021-05-20 16:01:53 +0200

    added lists model and functionality, not working so far

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'lists/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #lists
+  get '/lists', to: 'lists#index'
+  get '/list/:id', to: 'lists#show'
+  get '/list/new', to: 'lists#new'
+  post '/lists', to: 'lists#create'
+  #resource :list, only: [:index, :show, :create, :new]
+
+
 end
